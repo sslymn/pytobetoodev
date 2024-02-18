f94d2712031f9448c4bb06444cba9fadec1fc057
Süleyman Kulaksız <slymann89@outlook.com>
2024-02-18 15:22:48 +0300

    first commit

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,14 @@
+birinciSayi = int(input("Birinci Sayiyi Giriniz : "))
+ikinciSayi = int(input("İkinci Sayiyi Giriniz : "))
+ 
+if (birinciSayi > ikinciSayi):
+    kucuksayi = ikinciSayi
+else:
+    kucuksayi = birinciSayi
+for i in range(1,kucuksayi+1):
+    if (birinciSayi % i==0) and (ikinciSayi%i ==0):
+        ebob = i
+        ekok = (birinciSayi*ikinciSayi)//ebob
+
+print ("EBOB:", ebob)
+print ("EKOK:", ekok)
